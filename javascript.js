103d8fb20bc59b9a319d7d895ee9b1e8d0dbbaab
Lyn Chin <shawnLchin@gmail.com>
2023-05-05 00:27:00 -0400

    Add button functionality to change number of ggrid squares.

--- javascript.js
+++ javascript.js
@@ -10,39 +10,63 @@ footer.textContent = "ETCH-A-SKETCH";
 document.body.appendChild(footer);
 document.body.insertBefore(container, footer);
 
-// btn.addEventListener('click', prompt());
-
 //function to make grid
 function createGrid (x)
 {
-    for(let i=1; i<=x*x; i++)
-{
-    const square = document.createElement('div');
-    square.setAttribute('id', i);
-
-    let gridSpace = ((500/x)/500)*100;
-    console.log(gridSpace);
     
 
-    //box styling
-    square.style.boxSizing = 'border-box';
-    square.style.border = 'thin solid black';
-    square.style.height = gridSpace + '%';
-    square.style.width = gridSpace + '%';
+    for(let i=1; i<=x*x; i++)
+    {
 
-    //no flex growing/shrinking; basis = 100/16
-    square.style.flex = '1 1 auto';
+        //create squares
+        const square = document.createElement('div');
+        square.setAttribute('id', i);
 
-    //add to container
-    container.appendChild(square);
+        //initialize percentage of container side that each square occupies
+        let gridSpace = ((500/x)/500)*100;
+        // console.log(gridSpace);
+        
 
-    //change color with hover
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'lightsalmon';
-    })
-}   
+        //box styling
+        square.style.boxSizing = 'border-box';
+        square.style.border = 'thin solid black';
+        square.style.height = gridSpace + '%';
+        square.style.width = gridSpace + '%';
+
+        //no flex growing/shrinking; basis = 100/16
+        square.style.flex = '1 1 auto';
+
+        //add to container
+        container.appendChild(square);
+
+        //change color with hover
+        square.addEventListener('mouseover', () => 
+        {
+            square.style.backgroundColor = 'lightsalmon';
+        })
+    }   
 }
 
 //initial grid
-createGrid(30);
+createGrid(16);
 
+//default square grid value
+let size = 16;
+
+function ask ()
+{
+    //delete contents of grid container
+    while(container.firstChild)
+    {
+        container.removeChild(container.firstChild);
+    }
+    
+    size = prompt('What square size?');
+    createGrid(size);
+}
+
+//create node for size button
+const btn = document.getElementById('size');
+
+//pass reference to ask function
+btn.addEventListener('click', ask);
